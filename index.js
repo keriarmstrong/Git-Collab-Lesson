5ced17040e842dd99e83d2e51594f1a245d820e7
keriarmstrong <khetto@live.com>
2023-03-07 21:30:28 -0500

    what's his name

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function createPerson(name, age) {
     };
   }
   
-  function addPersonAsFriend(person, other) {
-    person.friends.push(other);
+  function addPersonAsFriend(person, someoneIKnow) {
+    person.friends.push(someoneIKnow);
     return person;
   }
   
